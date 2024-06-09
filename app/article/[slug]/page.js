3001dcec9257ec31662c242b2b9585d48cfe877e
GekiteniusY <genkichi.yogi@gmail.com>
2024-06-09 14:38:20 +0900

    generateStaticParamsを更新

--- app/article/[slug]/page.js
+++ app/article/[slug]/page.js
@@ -12,10 +12,19 @@ import styles from '../../../styles/post.module.css';
 // Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
   const database = await getDatabase();
-  return database?.map((page) => {
-    const slug = page.properties.Slug?.formula?.string;
-    return ({ id: page.id, slug });
-  });
+  return {
+    paths: database.map((page) => {
+      const slug = page.properties.Slug?.formula?.string;
+      return ({
+        params: { id: page.id, slug },
+      });
+    }),
+    fallback: false,
+  };
+  // return database?.map((page) => {
+  //   const slug = page.properties.Slug?.formula?.string;
+  //   return ({ id: page.id, slug });
+  // });
 }
 
 export default async function Page({ params }) {
@@ -49,28 +58,3 @@ export default async function Page({ params }) {
     </div>
   );
 }
-
-// export const getStaticPaths = async () => {
-//   const database = await getDatabase(databaseId);
-//   return {
-//     paths: database.map((page) => {
-//       const slug = page.properties.Slug?.formula?.string;
-//       return ({ params: { id: page.id, slug } });
-//     }),
-//     fallback: true,
-//   };
-// };
-
-// export const getStaticProps = async (context) => {
-//   const { slug } = context.params;
-//   const page = await getPage(id);
-//   const blocks = await getBlocks(id);
-
-//   return {
-//     props: {
-//       page,
-//       blocks,
-//     },
-//     revalidate: 1,
-//   };
-// };
